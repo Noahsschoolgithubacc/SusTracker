72328222fa2618ece1820696bff858e7bb76ffbd
Noah <Nphilli7@students.tulsatech.edu>
2024-11-11 14:43:20 -0600

    ContentView is started

--- SusTracker/View/ContentView.swift
+++ SusTracker/View/ContentView.swift
@@ -13,15 +13,39 @@ import SwiftUI
 // core data = framework that saves, tracks, filters, modifies data in ios apps, not a db, it connects the app to the db
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjContext
+    // fetch request = retrieves entities from a core data store
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var person: FetchedResults<SusPerson>
+    
+    @State private var showingAddView = false
+    
     var body: some View {
-        VStack {
-        
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("\(Int(totalMinutesToday())) minutes (Today)")
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                List {
+                    ForEach(person) { person in
+                        
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    // calcs the total minutes recorded
+    private func totalMinutesToday() -> Double {
+        var minutesToday: Double = 0
+        for item in person {
+            if Calendar.current.isDateInToday(item.date!) {
+                minutesToday += item.minutes
+            }
+        }
+        print("Total minutes today: \(minutesToday)")
+        return minutesToday
     }
 }
 
